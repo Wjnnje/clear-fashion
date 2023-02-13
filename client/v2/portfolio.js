3004eeeb27db890aee7e446303b623d91db2e040
M S <mathilde.salaun@edu.devinci.fr>
2023-02-13 18:28:44 +0100

    js commit

--- client/v2/portfolio.js
+++ client/v2/portfolio.js
@@ -26,6 +26,31 @@ const selectShow = document.querySelector('#show-select');
 const selectPage = document.querySelector('#page-select');
 const sectionProducts = document.querySelector('#products');
 const spanNbProducts = document.querySelector('#nbProducts');
+const spanNewProducts = document.querySelector('#nbNew');
+const spanNbBrands = document.querySelector('#nbBrands');
+const spanp50 = document.querySelector('#p50');
+const spanp90 = document.querySelector('#p90');
+const spanp95 = document.querySelector('#p95');
+const lastReleased = document.querySelector('#lastRelease');
+
+
+
+// Variables for Indicators
+// Brands
+var distinctBrands=[];
+// New Releases
+var w2_date=new Date();
+w2_date.setDate(w2_date.getDate() - 14);
+var recentProducts=[];
+// Reasonable Price
+var reasonablePrice={};
+distinctBrands.forEach(element => {
+  reasonablePrice[element]=true;
+});
+var allData;
+var allProducts=[];
+
+
 
 /**
  * Set global value
@@ -63,18 +88,47 @@ const fetchProducts = async (page = 1, size = 12) => {
 };
 
 /**
- * Render list of products
- * @param  {Array} products
+ * Fetch products from api
+ * @return {Object}
  */
-const renderProducts = products => {
+const fetchAll = async (page = 1, size = 12) => {
+  var finished=false;
+  var previous={};
+  while(!finished)
+  {
+    try {
+      const response = await fetch(
+        `https://clear-fashion-api.vercel.app?page=${page}&size=${size}`
+      );
+      const body = await response.json();
+  
+      if (body.success !== true) {
+        console.error(body);
+        return {currentProducts, currentPagination};
+      }
+      allProducts.push(body.data);
+      if (body.data==previous) finished=true;
+      previous=body.data;
+    
+    } catch (error) {
+      console.error(error);
+      return {currentProducts, currentPagination};
+    }
+  }
+  console.log(allProducts)
+  return allProducts
+};
+
+function renderAllProducts() {
   const fragment = document.createDocumentFragment();
   const div = document.createElement('div');
-  const template = products
+  
+  const template = allProducts
     .map(product => {
       return `
       <div class="product" id=${product.uuid}>
         <span>${product.brand}</span>
-        <a href="${product.link}">${product.name}</a>
+        <a href="${product.link}" target="Blank">${product.name}</a>
         <span>${product.price}</span>
       </div>
     `;
@@ -85,6 +139,92 @@ const renderProducts = products => {
   fragment.appendChild(div);
   sectionProducts.innerHTML = '<h2>Products</h2>';
   sectionProducts.appendChild(fragment);
+
+};
+
+/**
+ * Render list of products
+ * @param  {Array} products
+ */
+const renderProducts = products => {
+  const fragment = document.createDocumentFragment();
+  const div = document.createElement('div');
+  
+  const template = products
+    .map(product => {
+      return `
+      <div class="product" id=${product.uuid}>
+        <span>${product.brand}</span>
+        <a href="${product.link}" target="Blank">${product.name}</a>
+        <span>${product.price}</span>
+      </div>
+    `;
+    })
+    .join('');
+
+  div.innerHTML = template;
+  fragment.appendChild(div);
+  sectionProducts.innerHTML = '<h2>Products</h2>';
+  sectionProducts.appendChild(fragment);
+
+};
+
+
+function renderInfos (allProducts) {
+  
+//GET BRANDS (Feature2) / RECENT (Feature)
+  for (let i=0;i<allProducts.length;i++){
+    if (!Object.keys(distinctBrands).includes(allProducts[i].brand)) distinctBrands.push(allProducts[i].brand);
+    if (new Date(pallProducts[i].released)>w2_date) recentProducts.push(allProducts[i]);
+    if (allProducts[i].price>100) reasonablePrice[allProducts[i].brand]=false;
+  }
+  const nbBrands=distinctBrands.length;
+  const nbNew=recentProducts.length;
+  spanNbBrands.innerHTML = nbBrands;
+  spanNewProducts.innerHTML = nbNew;
+  
+  //GET PVALUES (Feature 10)
+  var brands_price=Object.assign([], products);;
+  brands_price.sort((a,b)=>{
+    if(a.price>b.price) return 1;
+    if(a.price<b.price) return -1;
+      return 0;
+  });
+  console.log(brands_price)
+  console.log(allProducts)
+  
+  var placep50=Math.round(0.5*brands_price.length)-1;
+  var p50=brands_price[placep50];
+  spanp50.innerHTML = p50.price;
+
+  
+  var placep90=Math.round(0.9*brands_price.length)-1;
+  var p90=brands_price[placep90]
+  spanp90.innerHTML = p90.price;
+
+  var placep95=Math.round(0.95*brands_price.length)-1;
+  var p95=brands_price[placep95]
+  spanp95.innerHTML = p95.price;
+  console.log(allProducts)
+};
+
+
+renderInfos(fetchAll());
+
+/**
+ * Render page selector
+ * @param  {Object} pagination
+ * @param  {Array}  distinctBrands
+ */
+const filterPage = pagination => {
+  const {currentPage, pageCount} = pagination;
+  const options = Array.from(
+    {'length': pageCount},
+    (value, index) => `<option value="${index + 1}">${index + 1}</option>`
+  ).join('');
+
+  selectPage.innerHTML = options;
+  selectPage.selectedIndex = currentPage - 1;
 };
 
 /**
@@ -104,13 +244,32 @@ const renderPagination = pagination => {
 
 /**
  * Render page selector
+ * @param  {Array} products
+ */
+const renderFilter = pagination => {
+  /*const {currentPage, pageCount} = pagination;
+  const options = Array.from(
+    {'length': pageCount},
+    (value, index) => `<option value="${index + 1}">${index + 1}</option>`
+  ).join('');
+
+  selectPage.innerHTML = options;
+  selectPage.selectedIndex = currentPage - 1;*/
+};
+
+/**
+ * Render page selector
  * @param  {Object} pagination
  */
 const renderIndicators = pagination => {
   const {count} = pagination;
 
   spanNbProducts.innerHTML = count;
+  
+  //spanNbBrands.innerHTML=nbBrands;
 };
+
+
 
 const render = (products, pagination) => {
   renderProducts(products);
@@ -138,3 +297,20 @@ document.addEventListener('DOMContentLoaded', async () => {
   setCurrentProducts(products);
   render(currentProducts, currentPagination);
 });
+
+/*selectShow.addEventListener('change', async (event) => {
+  const products = await fetchProducts(currentPagination.currentPage, parseInt(event.target.value));
+
+  setCurrentProducts(products);
+  render(currentProducts, currentPagination);
+});
+
+document.addEventListener('DOMContentLoaded', async () => {
+  const products = await fetchProducts();
+
+  setCurrentProducts(products);
+  render(currentProducts, currentPagination);
+});*/
+
+
+
