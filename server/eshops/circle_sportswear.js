548557899372da249b8cf9102e404cb165bfae8b
M S <mathilde.salaun@edu.devinci.fr>
2023-02-17 13:32:14 +0100

    commit scraping

---
+++ server/eshops/circle_sportswear.js
@@ -0,0 +1,58 @@
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
+
+/**
+ * Parse webpage e-shop
+ * @param  {String} data - html response
+ * @return {Array} products
+ */
+const parse = data => {
+  const $ = cheerio.load(data);
+
+  return $('.ProductGridContainer')
+    .map((i, element) => {
+      const name = $(element)
+        .find('.full-unstyled-link')
+        .text()
+        .trim()
+        .replace(/\s/g, ' ');
+        
+      const price = parseInt(
+        $(element)
+          .find('.price')
+          .text()
+      );
+      const color = $(element)
+        .find('.card__characteristic')
+        .text()
+        .trim()
+        .replace(/\s/g, ' ');
+
+      return {name, price, color};
+    })
+    .get();
+};
+
+/**
+ * Scrape all the products for a given url page
+ * @param  {[type]}  url
+ * @return {Array|null}
+ */
+module.exports.scrape = async url => {
+  try {
+    const response = await fetch(url);
+
+    if (response.ok) {
+      const body = await response.text();
+
+      return parse(body);
+    }
+
+    console.error(response);
+
+    return null;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
--- server/sandbox.js
+++ server/sandbox.js
@@ -1,13 +1,27 @@
 /* eslint-disable no-console, no-process-exit */
 const dedicatedbrand = require('./eshops/dedicatedbrand');
+const montlimartbrand = require('./eshops/montlimart');
+const circlebrand = require('./eshops/circle_sportswear');
 
-async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
+const link_dedicated="https://www.dedicatedbrand.com/en/men/news";
+const link_montlimart="https://www.montlimart.com/99-vetements";
+const link_circle="https://shop.circlesportswear.com/collections/all";
+
+async function sandbox (eshop = link_dedicated) {
   try {
     console.log(`🕵️‍♀️  browsing ${eshop} eshop`);
 
-    const products = await dedicatedbrand.scrape(eshop);
+    const products_dedicated = await dedicatedbrand.scrape(eshop);
+    const products_montlimart = await montlimartbrand.scrape(eshop);
+    const products_circle = await circlebrand.scrape(eshop);
 
-    console.log(products);
+
+    console.log('===============Dedicated=============');
+    console.log(products_dedicated);
+    console.log('===============Montlimart=============');
+    console.log(products_montlimart);
+    console.log('===============Circle Sportswear=============');
+    console.log(products_circle);
     console.log('done');
     process.exit(0);
   } catch (e) {
