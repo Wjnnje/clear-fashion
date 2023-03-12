d649cd93ad06d3c9098fe2ba0b421fa8c3f006f0
M S <mathilde.salaun@edu.devinci.fr>
2023-03-13 00:12:27 +0100

    json updates

--- server/db_node.js
+++ server/db_node.js
@@ -1,17 +1,19 @@
 const {MongoClient} = require('mongodb');
 const MONGODB_URI = 'mongodb+srv://mathildesalaun:atlas2972@cluster0.jea3vhl.mongodb.net?retryWrites=true&writeConcern=majority';
 const MONGODB_DB_NAME = 'clearfashion';
-
+const products=require('C:/Users/meama/Documents/A4/S2/EWA Web Application Architectures/clear-fashion/server/all_products.json');
 
 async function createDataBase()
 {
     const client = await MongoClient.connect(MONGODB_URI, {'useNewUrlParser': true});
-    const db =  client.db(MONGODB_DB_NAME)
-
-    const products = [];
+    const db =  client.db(MONGODB_DB_NAME);
 
     const collection = db.collection('products');
-    const result = collection.insertMany(products);
-
+    await collection.deleteMany({});
+    const result = await collection.insertMany(products);
+    console.log("products inserted !")
     console.log(result);
+    
+    client.close();
 }
+createDataBase();
