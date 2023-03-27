61cdb5a8fdb4866c45f022703e8f581ced59b7d5
M S <mathilde.salaun@edu.devinci.fr>
2023-03-27 13:36:03 +0200

    api

--- server/mongodb_api.js
+++ server/mongodb_api.js
@@ -1,8 +1,10 @@
+const {connect}=require('http2');
 const {MongoClient} = require('mongodb');
 const MONGODB_URI = 'mongodb+srv://mathildesalaun:atlas2972@cluster0.jea3vhl.mongodb.net?retryWrites=true&writeConcern=majority';
 const MONGODB_DB_NAME = 'clearfashion';
 const products=require('C:/Users/meama/Documents/A4/S2/EWA Web Application Architectures/clear-fashion/server/all_products.json');
-
+const {ObjectID}=require('bson');
+var ObjectId=require('mongodb').ObjectId;
 
 async function FetchProducts(limit=12, brand=undefined, maxPrice=undefined, partOfName=undefined, sortedByPrice=false, sortedByDate=false, recent2weeks=false){
     const client = await MongoClient.connect(MONGODB_URI, {'useNewUrlParser': true});
@@ -31,7 +33,7 @@ async function FetchProducts(limit=12, brand=undefined, maxPrice=undefined, part
         client.close();
         console.log(result);
         return result;
-    } catch(e){response.send({error : "id not valid !"});  }
+    } catch(e){response.send({error : "query not valid !"});  }
 }
 //FetchProducts();
 
@@ -40,7 +42,7 @@ async function FetchProductID(ID='640e69d4c433c3aa7ebbc895'){
     const db =  client.db(MONGODB_DB_NAME);
     const collection = db.collection('products');
     try{
-        const result=await collection.find({"ObjectId":ID}).toArray();
+        const result=await collection.find({_id:new ObjectId(ID.params.id)}).toArray();
         console.log(result);
         client.close();
         return(result);
