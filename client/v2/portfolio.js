d069f35d46c1886aa9d6a193df1fe2e079a69688
M S <mathilde.salaun@edu.devinci.fr>
2023-04-02 15:10:49 +0200

    portfolio cleaning

--- client/v2/portfolio.js
+++ client/v2/portfolio.js
@@ -41,19 +41,19 @@ const favs=document.querySelector('#select-Fav')
 
 // Variables for Indicators
 // Brands
-var distinctBrands=[];
+let distinctBrands=[];
 // New Releases
-var w2_date=new Date();
+let w2_date=new Date();
 w2_date.setDate(w2_date.getDate() - 14);
-var recentProducts=[];
+let recentProducts=[];
 // Reasonable Price
-var reasonablePrice={};
+let reasonablePrice={};
 distinctBrands.forEach(element => {
   reasonablePrice[element]=true;
 });
-var productLast;
-var allData;
-var allProducts=[];
+let productLast;
+let allData;
+let allProducts=[];
 
 
 
@@ -72,16 +72,19 @@ const setCurrentProducts = ({result, meta}) => {
  * Fetch products from api
  * @param  {Number}  [page=1] - current page to fetch
  * @param  {Number}  [size=12] - size of the page
+ * @param  {Number}  [limit=12]
+ * @param  {Number}  [price=60]
+ * @param  {}
  * @return {Object}
  */
 const fetchProducts = async (page = 1, size = 12) => {
   try {
-    const response = await fetch(
-      `https://clear-fashion-api.vercel.app?page=${page}&size=${size}`
-    );
     /*const response = await fetch(
-      'https://clear-fashion-ew7a.vercel.app?page=${page}&size=${size}'
+      `https://clear-fashion-api.vercel.app?page=${page}&size=${size}`
     );*/
+    const response = await fetch(
+      'https://clear-fashion-ew7a.vercel.app?page=${page}&size=${size}'
+    );
     const body = await response.json();
 
     if (body.success !== true) {
@@ -106,12 +109,12 @@ const fetchAll = async (page = 1, size = 12) => {
   while(!finished)
   {
     try {
-      const response = await fetch(
-        `https://clear-fashion-api.vercel.app?page=${page}&size=${size}`
-      );
       /*const response = await fetch(
-        'https://clear-fashion-ew7a.vercel.app?page=${page}&size=${size}'
+        `https://clear-fashion-api.vercel.app?page=${page}&size=${size}`
       );*/
+      const response = await fetch(
+        'https://clear-fashion-ew7a.vercel.app?page=${page}&size=${size}'
+      );
       const body = await response.json();
   
       if (body.success !== true) {
@@ -130,35 +133,6 @@ const fetchAll = async (page = 1, size = 12) => {
   console.log(allProducts)
   return allProducts
 };
-/*
-function renderAllProducts() {
-  const fragment = document.createDocumentFragment();
-  const div = document.createElement('div');
-  
-  const template = allProducts
-    .map(product => {
-      return `
-      <div class="product" id=${product.uuid}>
-        <span>${product.brand}</span>
-        <a href="${product.link}" target="Blank">${product.name}</a>
-        <span>${product.price}</span>
-      </div>
-    `;
-    })
-    .join('');
-
-  div.innerHTML = template;
-  fragment.appendChild(div);
-  sectionProducts.innerHTML = '<h2>Products</h2>';
-  sectionProducts.appendChild(fragment);
-
-};*/
-
-/**
- * Render list of products
- * @param  {Array} products
- */
-const testProducts = test => {console.log(products);};
 
 /**
  * Render list of products
