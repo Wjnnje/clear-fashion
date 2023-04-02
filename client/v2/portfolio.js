e5ed7255352b72c71fef4ec12d4dac70e367429e
M S <mathilde.salaun@edu.devinci.fr>
2023-04-02 20:23:55 +0200

    let instead if var

--- client/v2/portfolio.js
+++ client/v2/portfolio.js
@@ -72,7 +72,8 @@ const setCurrentProducts = ({result, meta}) => {
  * Fetch products from api
  * @param  {Number}  [page=1] - current page to fetch
  * @param  {Number}  [size=12] - size of the page
-
+ * @param  {Number}  [limit=12]
+ * 
  * @param  {Number}  [price=null]
  * @param  {Object}  [brand=null]
  * @param  {Object}  [name=null]
@@ -81,7 +82,7 @@ const setCurrentProducts = ({result, meta}) => {
  * @param  {Boolean} [favorite=false]
  * @return {Object}
  */
-const fetchProducts = async (page = 1, size = 12, price=null, brand=null, name=null, color=null, favorite=false, sortedBy='price-asc') => {
+const fetchProducts = async (page = 1, size = 12, limit=12, price=null, brand=null, name=null, color=null, favorite=false, sortedBy='price-asc') => {
   try {
     /*const response = await fetch(
       `https://clear-fashion-api.vercel.app?page=${page}&size=${size}`
